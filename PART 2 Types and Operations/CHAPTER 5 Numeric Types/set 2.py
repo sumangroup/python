d7b0a49575a2a56e756f182d50394f4656f43891
Raju Mane <mail.sumangroup@gmail.com>
2025-01-03 18:09:12 +0530

    set1,set2,set3

---
+++ PART 2 Types and Operations/CHAPTER 5 Numeric Types/set 2.py
@@ -0,0 +1,17 @@
+# set
+s1={}
+print('s1',type(s1))
+s2=set()
+print('s2',type(s2))
+s3=[1,2,3,3,1,2]
+print('s3',s3)
+s4=set(s3)
+print('s4',s4)
+s5='Raju'
+print('s5',s5)
+s6=set(s5)
+print('s6',s6)
+s7=(1,5,6)
+print('s7',s7)
+s8=set(s7)
+print('s8',s8)
